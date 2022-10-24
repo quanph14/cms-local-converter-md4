444e414fa15df281dd226e0eb1901e50d009b2c3
quanph14 <phquan.0493@gmail.com>
2022-10-24 14:29:17 +0700

    done 2410

---
+++ src/main/java/com/codegym/formatter/ProvinceFormatter.java
@@ -0,0 +1,35 @@
+package com.codegym.formatter;
+
+import com.codegym.model.Province;
+import com.codegym.service.province.IProvinceService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.Formatter;
+import org.springframework.stereotype.Component;
+
+import java.text.ParseException;
+import java.util.Locale;
+import java.util.Optional;
+
+@Component
+//Lớp ProvinceFormatter sử dụng provinceService để chuyển đổi từ id của province sang object của province.
+public class ProvinceFormatter implements Formatter<Province> {
+    private IProvinceService provinceService;
+
+    @Autowired
+    public ProvinceFormatter(IProvinceService provinceService) {
+        this.provinceService = provinceService;
+    }
+
+    //Phương thức parse() chuyển một String sang kiểu dữ liệu đích sử dụng một Locale xác định.
+    // Phương thức print() thì thực hiện công việc ngược lại, chuyển đổi từ một đối tượng của kiểu dữ liệu đích sang String.
+    @Override
+    public Province parse(String text, Locale locale) throws ParseException {
+        Optional<Province> provinceOptional = provinceService.findById(Long.parseLong(text));
+        return provinceOptional.orElse(null);
+    }
+
+    @Override
+    public String print(Province object, Locale locale) {
+        return "[" + object.getId() + ", " +object.getName() + "]";
+    }
+}
